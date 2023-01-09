07331e45e314e4202b11bcef89de39213d76ee1e
gebo-10 <937319738@qq.com>
2023-01-09 23:45:26 +0800

    ktest

---
+++ packages/k/ktest/xmake.lua
@@ -0,0 +1,19 @@
+package("ktest")
+    set_kind("binary")
+    set_description("The ktest package")
+
+    add_urls("https://github.com/gebo-10/kpackage.git")
+    add_versions("1.0", "<shasum256 or gitcommit>")
+
+    on_install(function (package)
+        local configs = {}
+        if package:config("shared") then
+            configs.kind = "shared"
+        end
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        -- TODO check includes and interfaces
+        -- assert(package:has_cfuncs("foo", {includes = "foo.h"})
+    end)
