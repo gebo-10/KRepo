684900d73cfdc5734f1e82e456453f0cdad7f460
jiajun.qiu <jiajun.qiu@horizon.cc>
2024-09-26 17:28:08 +0800

    imgui

--- packages/k/kpackage-imgui/xmake.lua
+++ packages/k/kpackage-imgui/xmake.lua
@@ -11,7 +11,7 @@ package("kpackage-imgui")
     add_configs("vulkan",           {description = "Enable the vulkan backend", default = false, type = "boolean"})
     add_configs("sdl2",             {description = "Enable the sdl2 backend with sdl2_renderer", default = false, type = "boolean"})
     
-    add_includedirs("include", "backends")
+    add_includedirs("include", "include/backends")
 
     on_install("windows", function (package)
         local configs = {
@@ -22,5 +22,6 @@ package("kpackage-imgui")
         }
 
         os.cp(path.join(package:scriptdir(), "port", "xmake.lua"), "xmake.lua")
+        os.cp("backends/*.h",package:installdir("include/backends"))
         import("package.tools.xmake").install(package, configs)
     end)
