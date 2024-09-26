0487f87927d568b245c702561dc1a5bc64f65599
gebo <937319738@qq.com>
2024-09-27 00:05:45 +0800

    u

--- packages/k/kpackage-wgpu-native/xmake.lua
+++ packages/k/kpackage-wgpu-native/xmake.lua
@@ -1,21 +1,9 @@
 package("kpackage-wgpu-native")
     set_homepage("https://github.com/gfx-rs/wgpu-native")
     set_description("Native WebGPU implementation based on wgpu-core")
-    set_license("Apache-2.0")
 
-    --if is_plat("windows") and is_arch("x64") then
-        --if is_debug() then
-            add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/v22.1.0.5/wgpu-windows-x86_64-msvc-debug.zip")
-            add_versions("22.1.0.5", "4D63E16941B7ABD8491ACBABF78B3F74A52025276A2A8FADF22E7BD20C1540BE")
-        -- else
-        --     add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/$(version)/wgpu-windows-x86_64-msvc-release.zip")
-        --     add_versions("v22.1.0.5", "9e1591d60c2d2ee20d6d4a63bc01c7c5eecf7734761673160aa639e550a1ba4d")
-        -- end
-    --end
-
-    -- if is_plat("windows") then
-    --     add_configs("vs_runtime", {description = "Set vs compiler runtime.", default = "MD", readonly = true})
-    -- end
+    add_urls("https://github.com/gfx-rs/wgpu-native/releases/download/v22.1.0.5/wgpu-windows-x86_64-msvc-debug.zip")
+    add_versions("22.1.0.5", "4D63E16941B7ABD8491ACBABF78B3F74A52025276A2A8FADF22E7BD20C1540BE")
 
     add_includedirs("include", "include/webgpu","include/wgpu")
 
