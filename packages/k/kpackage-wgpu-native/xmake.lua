3f4932545511d86661c02d381c75b9a75f9cdb15
gebo <937319738@qq.com>
2024-09-27 00:21:39 +0800

    u

--- packages/k/kpackage-wgpu-native/xmake.lua
+++ packages/k/kpackage-wgpu-native/xmake.lua
@@ -26,7 +26,7 @@ package("kpackage-wgpu-native")
     --add_urls("https://github.com/facebook/yoga.git")
 
     set_urls("https://github.com/gfx-rs/wgpu-native/releases/download/v22.1.0.5/wgpu-windows-x86_64-msvc-debug.zip")
-    add_versions("3.1.0","4D63E16941B7ABD8491ACBABF78B3F74A52025276A2A8FADF22E7BD20C1540BE")
+    add_versions("3.1.0","4d63e16941b7abd8491acbabf78b3f74a52025276a2a8fadf22e7bd20c1540be")
     on_install("windows", function (package)
         os.cp("include", package:installdir())
         --(package:debug() and "Debug" or "Release")
